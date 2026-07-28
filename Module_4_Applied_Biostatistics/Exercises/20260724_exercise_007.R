a2cd5ef3efde6fc82feabae9730fd8cd1f5879c2
sven_meinerz <sv.meinerz@gmail.com>
2026-07-28 08:25:07 +0200

    Pivot

--- Module_4_Applied_Biostatistics/Exercises/20260724_exercise_007.R
+++ Module_4_Applied_Biostatistics/Exercises/20260724_exercise_007.R
@@ -3,6 +3,23 @@ pacman::p_load(
   readxl, readODS, foreign, haven
 )
 
+pacman::p_load(
+  conflicted, tidyverse, here,
+  grid, gridExtra, car,
+  ggsci, ggsignif, ggthemes, ggridges,
+  # gganimate,
+  ggforce,
+  ggbeeswarm,
+  wrappedtools,
+  # emojifont,
+  patchwork,
+  GGally
+)
+conflicts_prefer(
+  dplyr::filter,
+  ggplot2::mean_cl_boot
+) # solves name conflict
+
 # Exercise ####
 # Import the data as 3 tibbles
 # First, the position of the data is defined
@@ -80,12 +97,42 @@ mean_GFP <- (rawdata[[1, 4]] + rawdata[[2, 4]] + rawdata[[3, 4]])/3
 rawdata <- mutate(.data = rawdata,
                   normalized = rawdata$GFP / mean_GFP)
 
+# Old Data 2 ####
+# Lets do another table
 
+excel_positions <- c("B3:E8", "B10:E15", "B17:E22")
+for (untidy_i in seq_along(excel_positions)) {
+  assign(paste0("untidy_", untidy_i), read_excel(
+    path = here("Data/Auswetung_FLO11.xlsx"),
+    range = excel_positions[untidy_i],
+    col_names = c("Strain", "HA", "FLAG", "SYN8")
+  )
+  )
+}
 
+untidy_combined <- bind_rows(untidy_1, untidy_2, untidy_3,
+                             .id = "Replicant")
+rawdata <- untidy_combined
 
+rawdata <- mutate(.data = rawdata,
+                  `HA/SYN8` = rawdata$HA / rawdata$SYN8,
+                  `FLAG/SYN8` = rawdata$FLAG / rawdata$SYN8)
 
+norm_factor_HA <- mean(c(rawdata[[3,6]], rawdata[[9,6]], rawdata[[15,6]]))
+norm_factor_FLAG <- mean(c(rawdata[[3,7]], rawdata[[9,7]], rawdata[[15,7]]))
 
+rawdata <- mutate(.data = rawdata,
+                  `norm. HA` = rawdata$`HA/SYN8` / norm_factor_HA,
+                  `norm. FLAG` = rawdata$`FLAG/SYN8` / norm_factor_FLAG)
 
+rawdata_wide <- pivot_wider(rawdata,
+                            values_from = c(HA, FLAG, SYN8, `HA/SYN8`, `FLAG/SYN8`, `norm. HA`, `norm. FLAG`),
+                            names_from = Strain,
+                            names_glue = "{.value}@{Strain}")
 
+my_colors <- c("#fde725", "#7ad151", "#22a884", "#2a788e", "#414487", "#440154")
 
-
+ggplot(rawdata, aes(x = factor(Strain), y = `norm. HA`, fill = Strain)) +
+  geom_bar(stat = "summary", fun = "mean") +
+  geom_beeswarm(cex = 2, size = 3, alpha = 0.5) +
+  scale_fill_manual(values = my_colors)
